6190ac152d59529212e27b7196e1655db0d7bd8e
ysfakz <yusuf12pegasus@live.com>
2024-04-29 14:54:47 +0200

    loading screen fix

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -8,10 +8,16 @@ public class MainMenuUI : MonoBehaviour {
 
     [SerializeField] private Button playButton;
     [SerializeField] private Button quitButton;
+    [SerializeField] private Transform loadingScreen;
+
+    private void Awake() {
+        loadingScreen.gameObject.SetActive(false);
+    }
 
     private void Update() {
         playButton.onClick.AddListener(() => {
             Loader.Load(Loader.Scene.GameScene);
+            loadingScreen.gameObject.SetActive(true);
             // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         });
         quitButton.onClick.AddListener(() => {
